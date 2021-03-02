d57c828966bd8f5e580b4c1040b5e33c65722ea9
Peggy Wang <65714873+Peggy-Wang@users.noreply.github.com>
2021-03-02 15:53:51 +0800

    second commit

--- ZhihuDaily/LatestNewsCell.swift
+++ ZhihuDaily/LatestNewsCell.swift
@@ -11,29 +11,33 @@ struct LatestNewsCell: View {
     var topStory: LatestTopStory
     
     var body: some View {
-        NavigationLink(destination: NewsDetailsPage(details: newsDetails9733310)) {
-            ZStack(alignment: .bottomLeading) {
-                ImageBlurView(imageUrl: URL(string: topStory.image)!)
-                VStack(alignment: .leading) {
-                    Text(topStory.title)
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .padding(.leading)
-                        .padding(.trailing)
-                    
-                    Text(topStory.hint)
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding(.top, 5)
-                        .padding(.leading)
-                        .padding(.trailing)
-                }
-                .frame(width: UIScreen.main.bounds.width, height: 120, alignment: .leading)
+        ZStack(alignment: .bottomLeading) {
+            ImageBlurView(imageUrl: URL(string: topStory.image)!)
+            VStack(alignment: .leading) {
+                Text(topStory.title)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding(.leading)
+                    .padding(.trailing)
+                
+                Text(topStory.hint)
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding(.top, 5)
+                    .padding(.leading)
+                    .padding(.trailing)
             }
-            .frame(width: UIScreen.main.bounds.width,
-                   height: UIScreen.main.bounds.width)
+            .frame(width: UIScreen.main.bounds.width, height: 120, alignment: .leading)
         }
+        .frame(width: UIScreen.main.bounds.width,
+               height: UIScreen.main.bounds.width)
+        .onTapGesture {
+            showTopStory = true
+        }
+        .fullScreenCover(isPresented: $showTopStory, content: {
+            NewsDetailsPage(details: newsDetails9733310)
+        })
     }
 }
 
