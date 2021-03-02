b7ef3406353fa7f86193c3fb38c982234b75bfc7
Peggy Wang <65714873+Peggy-Wang@users.noreply.github.com>
2021-03-02 15:49:54 +0800

    second commit

--- ZhihuDaily/NewsCell.swift
+++ ZhihuDaily/NewsCell.swift
@@ -11,33 +11,37 @@ struct NewsCell: View {
     let story: Story
     
     var body: some View {
-        NavigationLink(destination: NewsDetailsPage(details: newsDetails9733310)) {
-            HStack {
-                VStack(alignment: .leading) {
-                    Text(story.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color(.label))
-                        .padding(5)
+        HStack {
+            VStack(alignment: .leading) {
+                Text(story.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(.label))
+                    .padding(5)
                 
-                    Text(story.hint)
-                        .font(.caption)
-                        .foregroundColor(.secondary)
-                        .padding(5)
-                    
-                    Spacer()
-                }
-                .padding(.top)
-                .padding(.leading, 5)
+                Text(story.hint)
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                    .padding(5)
                 
                 Spacer()
-                
-                Image(uiImage: loadImageData(url: URL(string: story.images[0])!))
-                    .resizable()
-                    .frame(width: 80, height: 80)
-                    .padding()
             }
-            .frame(width: UIScreen.main.bounds.width, height: 110)
+            .padding(.top)
+            .padding(.leading, 5)
+            
+            Spacer()
+            
+            Image(uiImage: loadImageData(url: URL(string: story.images[0])!))
+                .resizable()
+                .frame(width: 80, height: 80)
+                .padding()
         }
+        .frame(width: UIScreen.main.bounds.width, height: 110)
+        .onTapGesture {
+            showNewsDetails = true
+        }
+        .fullScreenCover(isPresented: $showNewsDetails, content: {
+            NewsDetailsPage(details: newsDetails9733310)
+        })
     }
 }
 
